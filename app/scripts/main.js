f147e65ddfc6643bc76af80b5eefe897ced5449d
ira-kabalyuk <jokky@bigmir.net>
2016-11-28 22:36:23 +0200

    form dev

---
+++ app/scripts/main.js
@@ -0,0 +1,27 @@
+console.log('\'Allo \'Allo!');
+
+ 
+jQuery(document).ready(function() {   
+
+    /* placeholder*/       
+    $('input, textarea').each(function(){
+        var placeholder = $(this).attr('placeholder');
+        $(this).focus(function(){ $(this).attr('placeholder', '');});
+        $(this).focusout(function(){             
+            $(this).attr('placeholder', placeholder);           
+        });
+    });
+    /* placeholder*/ 
+
+});
+
+
+ 
+
+
+
+
+
+
+
+
